614f69d8010db8dfee89a21914a86ad07c610968
Nekomata Matamune <nekomatamune995@gmail.com>
2019-10-26 21:11:41 +0800

    Comments for FxExt.kt

--- src/main/kotlin/me/nekomatamune/ygomaker/fx/FxExt.kt
+++ src/main/kotlin/me/nekomatamune/ygomaker/fx/FxExt.kt
@@ -4,6 +4,13 @@ import javafx.event.Event
 import javafx.event.EventHandler
 import javafx.scene.control.*
 
+/**
+ * Wraps [listener] into a [javafx.beans.value.ChangeListener] to listens to
+ * value changed event of [this].
+ *
+ * [listener] is expected to get the updated value from the receiver itself
+ * directly.
+ */
 fun Control.addSimpleListener(listener: () -> Unit) {
 	when (this) {
 		is TextInputControl -> this.textProperty()
@@ -15,5 +22,12 @@ fun Control.addSimpleListener(listener: () -> Unit) {
 	}.addListener { _, _, _ -> listener() }
 }
 
+/**
+ * Converts [this] into an [EventHandler].
+ */
 fun <T : Event> (() -> Unit).asEventHandler() = EventHandler<T> { this() }
+
+/**
+ * Converts [this] into an [EventHandler].
+ */
 fun <T : Event> ((T) -> Unit).asEventHandler() = EventHandler<T> { this(it) }
