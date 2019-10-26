f1cf33ac7cad326f0adec989bead7b538a8847e6
Nekomata Matamune <nekomatamune995@gmail.com>
2019-10-26 21:28:09 +0800

    Remove unnecessary Window controller

--- src/main/kotlin/me/nekomatamune/ygomaker/fx/App.kt
+++ src/main/kotlin/me/nekomatamune/ygomaker/fx/App.kt
@@ -6,6 +6,10 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
+import me.nekomatamune.ygomaker.Command
+import me.nekomatamune.ygomaker.Event
+import me.nekomatamune.ygomaker.EventName
+import me.nekomatamune.ygomaker.dispatchEvent
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger { }
@@ -17,6 +21,11 @@ class App : Application() {
 		val appPane = FXMLLoader().apply {
 			location = Resources.getResource("fx/Window.fxml")
 		}.load<BorderPane>()
+
+		dispatchEvent(Event(
+			name = EventName.LOAD_PACK,
+			packDir = Command.dataDir.resolve(Command.packCode)
+		))
 
 		logger.info { "Showing stage" }
 		primaryStage.apply {
