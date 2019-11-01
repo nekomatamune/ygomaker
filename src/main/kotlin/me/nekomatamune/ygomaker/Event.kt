0d0164c16d1c7144d53a8e773d0dcb6c94ff74e2
Nekomata Matamune <nekomatamune995@gmail.com>
2019-11-02 00:02:11 +0800

    Comments for Event and each event type

--- src/main/kotlin/me/nekomatamune/ygomaker/Event.kt
+++ src/main/kotlin/me/nekomatamune/ygomaker/Event.kt
@@ -1,11 +1,14 @@
 package me.nekomatamune.ygomaker
 
 import javafx.event.ActionEvent
-import mu.KotlinLogging
 import java.nio.file.Path
 
 typealias EventHandler = (Event) -> Result<Unit>
 
+/**
+ * Encapsulates the type and payloads of an event. What payloads are set depends
+ * on the [Event.name].
+ */
 data class Event(
 	val name: EventName = EventName.UNKNOWN,
 
@@ -18,13 +21,22 @@ data class Event(
 	val image: Image? = null
 )
 
+/**
+ * List of events that can be fired by the UI components.
+ */
 enum class EventName {
 	UNKNOWN,
+	/** Should also set [Event.packDir] */
 	LOAD_PACK,
+	/** Should also set [Event.packDir] */
 	SAVE_PACK,
+	/** Should also set [Event.packDir] */
 	SAVE_PACK_AS,
+	/** Should also set [Event.card] */
 	SELECT_CARD,
+	/** Should also set [Event.card] */
 	MODIFY_CARD,
+	/** Should also set [Event.image] */
 	MODIFY_CARD_IMAGE,
 }
 
