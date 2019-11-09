5427833af2db64a71fc1a66b34091269480d0735
Nekomata Matamune <nekomatamune995@gmail.com>
2019-11-10 00:38:47 +0800

    Add dummy card rendere controller

--- src/main/kotlin/me/nekomatamune/ygomaker/fx/CardRenderer.kt
+++ src/main/kotlin/me/nekomatamune/ygomaker/fx/CardRenderer.kt
@@ -1,4 +1,18 @@
 package me.nekomatamune.ygomaker.fx
 
+import javafx.fxml.FXML
+import javafx.scene.image.ImageView
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {  }
+
 class CardRenderer {
+
+	@FXML private lateinit var imageView: ImageView
+
+	@FXML
+	fun initialize() {
+		logger.debug { "Initializing CardRenderer" }
+	}
+
 }
