6c8280a2ec936e776712bafb45e30d747c6756bc
Nekomata Matamune <nekomatamune995@gmail.com>
2019-11-11 22:46:23 +0800

    Render card frame

--- src/main/kotlin/me/nekomatamune/ygomaker/fx/CardRenderer.kt
+++ src/main/kotlin/me/nekomatamune/ygomaker/fx/CardRenderer.kt
@@ -2,46 +2,55 @@ package me.nekomatamune.ygomaker.fx
 
 import javafx.fxml.FXML
 import javafx.scene.canvas.Canvas
-import javafx.scene.image.Image
-import javafx.scene.image.ImageView
 import javafx.scene.layout.BorderPane
-import javafx.scene.paint.Color
-import me.nekomatamune.ygomaker.Event
-import me.nekomatamune.ygomaker.EventType
-import me.nekomatamune.ygomaker.dispatcher
-import me.nekomatamune.ygomaker.success
+import me.nekomatamune.ygomaker.*
 import mu.KotlinLogging
 
 
-private val logger = KotlinLogging.logger {  }
+private val logger = KotlinLogging.logger { }
 
 class CardRenderer {
 
 	@FXML private lateinit var rootPane: BorderPane
 
+	private lateinit var card: Card
+
 	@FXML
 	fun initialize() {
 		logger.debug { "Initializing CardRenderer" }
 
-		dispatcher.register(EventType.MODIFY_CARD) {
-			onModifyCard(it)
-		}
 		dispatcher.register(EventType.SELECT_CARD) {
-			onModifyCard(it)
+			card = it.card!!
+			render()
 		}
+
+		dispatcher.register(EventType.MODIFY_CARD) {
+			card = it.card!!.copy(image = card.image)
+			render()
+		}
+
+		dispatcher.register(EventType.MODIFY_CARD_IMAGE) {
+			card = card.copy(image = it.image!!)
+			render()
+		}
+
 	}
 
-	private fun onModifyCard(event: Event): Result<Unit> {
+	private fun render(): Result<Unit> {
+		logger.info { "Render card" }
 
-		val canvas = Canvas(200.0, 400.0)
+		val canvas = Canvas(400.0, 570.0)
 		val gc = canvas.graphicsContext2D
 
-		gc.stroke = Color.RED
-		gc.strokeRect(10.0, 10.0, 20.0, 20.0)
+		val cardFrameImage = getCardFrame(card).onFailure {
+			return Result.failure(it)
+		}
 
+		gc.drawImage(cardFrameImage.getOrNull()!!, 0.0, 0.0, 400.0, 570.0)
 
 		rootPane.center = canvas
 
 		return Result.success()
+
 	}
 }
