f90e441435782eb2e4c04f9517676fb31bf3fd60
Nekomata Matamune <nekomatamune995@gmail.com>
2020-01-05 21:09:11 -0800

    Set image

--- src/test/kotlin/me/nekomatamune/ygomaker/fx/CardFormSpec.kt
+++ src/test/kotlin/me/nekomatamune/ygomaker/fx/CardFormSpec.kt
@@ -18,17 +18,21 @@ import org.testfx.api.FxToolkit
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 import strikt.assertions.isTrue
+import java.nio.file.Paths
 
 object CardFormSpec : Spek({
 	val robot = FxRobot()
 	lateinit var app: Application
 	lateinit var ctrl: CardForm
-	lateinit var pane: GridPane
 
 	lateinit var card: Card
 
-	var imageModifiedHandler = slot<ImageModifiedHandler>()
 	val mockCardImage = mockk<CardImage>(relaxed = true)
+	val capturedImageModifiedHandler = slot<ImageModifiedHandler>()
+	every {
+		mockCardImage.imageModifiedHandler = capture(
+			capturedImageModifiedHandler)
+	}.just(Runs)
 
 	beforeGroup {
 		FxToolkit.registerPrimaryStage()
@@ -42,16 +46,13 @@ object CardFormSpec : Spek({
 	}
 
 	beforeEachTest {
-		every {
-			mockCardImage.imageModifiedHandler = capture(imageModifiedHandler)
-		}.just(Runs)
+		lateinit var loader: FXMLLoader
 
 		app = FxToolkit.setupApplication {
 			object : Application() {
 				override fun start(primaryStage: Stage) {
-					FXMLLoader().apply {
+					loader = FXMLLoader().apply {
 						location = Resources.getResource("fx/CardForm.fxml")
-
 						controllerFactory = Callback<Class<*>, Any> {
 							when (it) {
 								CardImage::class.java -> mockCardImage
@@ -59,91 +60,102 @@ object CardFormSpec : Spek({
 								else -> throw UnsupportedOperationException(it.toString())
 							}
 						}
-					}.let {
-						pane = it.load()
-						ctrl = it.getController()
 					}
-
-					primaryStage.scene = Scene(pane)
+					primaryStage.scene = Scene(loader.load<GridPane>())
 					primaryStage.show()
 				}
 			}
 		}
 
-
-
-
-		card = ctrl.card
-		ctrl.cardModifiedHandler = {
-			card = it.copy()
-		}
+		ctrl = loader.getController()
+		ctrl.cardModifiedHandler = { card = it.copy() }
 	}
 
 	afterEachTest {
 		FxToolkit.cleanupApplication(app)
 	}
 
-	test("Should modify card name") {
-		robot.clickOn("#cardNameTextField")
-		robot.write("Hello World!")
-		expectThat(card.name).isEqualTo("Hello World!")
-	}
+	group("Basic") {
+		test("Should modify card name") {
+			robot.clickOn("#cardNameTextField")
+			robot.write("Hello World!")
+			expectThat(card.name).isEqualTo("Hello World!")
+		}
 
-	test("Should modify card type") {
-		robot.clickOn("#cardTypeComboBox")
-		robot.type(DOWN, CardType.SPECIAL_SUMMON_MONSTER.ordinal)
-		robot.type(ENTER)
-		expectThat(card.type).isEqualTo(CardType.SPECIAL_SUMMON_MONSTER)
-	}
+		test("Should modify card type") {
+			robot.clickOn("#cardTypeComboBox")
+			robot.type(DOWN, CardType.SPECIAL_SUMMON_MONSTER.ordinal)
+			robot.type(ENTER)
+			expectThat(card.type).isEqualTo(CardType.SPECIAL_SUMMON_MONSTER)
+		}
 
-	test("Should modify attribute") {
-		robot.clickOn("#attributeComboBox")
-		robot.type(DOWN, Attribute.WATER.ordinal)
-		robot.type(ENTER)
-		expectThat(card.monster?.attribute).isEqualTo(Attribute.WATER)
-	}
+		test("Should modify attribute") {
+			robot.clickOn("#attributeComboBox")
+			robot.type(DOWN, Attribute.WATER.ordinal)
+			robot.type(ENTER)
+			expectThat(card.monster?.attribute).isEqualTo(Attribute.WATER)
+		}
 
-	test("Should modify level") {
-		robot.clickOn("#levelComboBox")
-		robot.type(DOWN, 3)
-		robot.type(ENTER)
-		expectThat(card.monster?.level).isEqualTo(4)
-	}
+		test("Should modify level") {
+			robot.clickOn("#levelComboBox")
+			robot.type(DOWN, 3)
+			robot.type(ENTER)
+			expectThat(card.monster?.level).isEqualTo(4)
+		}
 
-	test("Should modify monster type") {
-		robot.clickOn("#monsterTypeComboBox")
-		robot.type(DOWN, 10)
-		robot.type(ENTER)
-		expectThat(card.monster?.type).isEqualTo(MONSTER_TYPE_PRESETS[10])
-	}
+		test("Should modify monster type") {
+			robot.clickOn("#monsterTypeComboBox")
+			robot.type(DOWN, 10)
+			robot.type(ENTER)
+			expectThat(card.monster?.type).isEqualTo(MONSTER_TYPE_PRESETS[10])
+		}
 
-	test("Should modify monster effect checker") {
-		robot.clickOn("#effectCheckBox")
-		expectThat(card.monster?.effect).isTrue()
-	}
+		test("Should modify monster effect checker") {
+			robot.clickOn("#effectCheckBox")
+			expectThat(card.monster?.effect).isTrue()
+		}
 
-	test("Should modify effect") {
-		robot.clickOn("#effectTextArea")
-		robot.write("First line.\n")
-		robot.write("Second line.")
-		expectThat(card.effect).isEqualTo("First line.\nSecond line.")
-	}
+		test("Should modify effect") {
+			robot.clickOn("#effectTextArea")
+			robot.write("First line.\n")
+			robot.write("Second line.")
+			expectThat(card.effect).isEqualTo("First line.\nSecond line.")
+		}
 
-	test("Should modify atk/def") {
-		robot.clickOn("#atkTextField")
-		robot.write("3000")
-		robot.clickOn("#defTextField")
-		robot.write("2500")
-		expectThat(card).get {
-			expectThat(monster?.atk).isEqualTo("3000")
-			expectThat(monster?.def).isEqualTo("2500")
+		test("Should modify atk/def") {
+			robot.clickOn("#atkTextField")
+			robot.write("3000")
+			robot.clickOn("#defTextField")
+			robot.write("2500")
+			expectThat(card).get {
+				expectThat(monster?.atk).isEqualTo("3000")
+				expectThat(monster?.def).isEqualTo("2500")
+			}
 		}
 	}
 
-	test("Should update image when modified") {
-		val image = Image(file = "my_file", x = 123, y = 456, size = 789)
-		imageModifiedHandler.invoke(image)
-		expectThat(card.image).isEqualTo(image)
+	group("Interaction with CardImage") {
+		test("Should update image when modified") {
+
+
+			val image = Image(file = "my_file", x = 123, y = 456, size = 789)
+			capturedImageModifiedHandler.captured(image)
+			expectThat(card.image).isEqualTo(image)
+		}
+
+		test("Should set image") {
+			val card = Card(
+				image = Image(file = "my_file", x = 123, y = 456, size = 777))
+			val packDir = Paths.get("my_path")
+
+			Platform.runLater {
+				ctrl.setState(card = card, packDir = packDir)
+				verify { mockCardImage.setImage(card.image!!, packDir) }
+			}
+
+
+		}
 	}
+
 
 })
