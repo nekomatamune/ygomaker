1f37a4b10e05b9ce4c793c64c56b7020a2c13794
Nekomata Matamune <nekomatamune995@gmail.com>
2020-02-25 01:29:35 -0800

    Switch between COMMAND and WINDOWS based on os

--- src/test/kotlin/me/nekomatamune/ygomaker/fx/AppCtrlSpec.kt
+++ src/test/kotlin/me/nekomatamune/ygomaker/fx/AppCtrlSpec.kt
@@ -9,6 +9,9 @@ import org.spekframework.spek2.Spek
 import org.testfx.api.FxRobot
 import java.nio.file.Paths
 
+private val SUPER = if (System.getProperty(
+				"os.name") == "Mac OS X") KeyCode.COMMAND else KeyCode.WINDOWS
+
 object AppCtrlSpec : Spek({
 	val mockCommand = mockk<Command>(relaxed = true)
 	val mockCardListCtrl = mockk<CardListCtrl>(relaxed = true)
@@ -41,28 +44,28 @@ object AppCtrlSpec : Spek({
 	group("#MenuBar") {
 		group("#LoadPackMenuItem") {
 			test("Should ask CardList to load pack") {
-				robot.press(KeyCode.COMMAND, KeyCode.O)
+				robot.press(SUPER, KeyCode.O)
 				verify { mockCardListCtrl.loadPack(null) }
 			}
 		}
 
 		group("#SavePackMenuItem") {
 			test("Should ask CardList to save pack") {
-				robot.press(KeyCode.COMMAND, KeyCode.S)
+				robot.press(SUPER, KeyCode.S)
 				every { mockCardListCtrl.savePack() }
 			}
 		}
 
 		group("#SavePackAsMenuItem") {
 			test("Should ask CardList to save pack as") {
-				robot.press(KeyCode.COMMAND, KeyCode.SHIFT, KeyCode.S)
+				robot.press(SUPER, KeyCode.SHIFT, KeyCode.S)
 				every { mockCardListCtrl.savePackAs() }
 			}
 		}
 
 		group("#NewCardMenuItem") {
 			test("Should ask CardList to add new card") {
-				robot.press(KeyCode.COMMAND, KeyCode.N)
+				robot.press(SUPER, KeyCode.N)
 				every { mockCardListCtrl.addCard() }
 			}
 		}
@@ -71,3 +74,4 @@ object AppCtrlSpec : Spek({
 	}
 
 })
+
